d255a2c342bee58d10a8a683bf2032221679cb87
Mariia Strykalo <miam.versa@gmail.com>
2025-08-18 17:20:38 +0300

    Add files via upload

---
+++ function_variables/results_list.py
@@ -0,0 +1,20 @@
+results = ["Mario", "Luigi", "Princess", "Yoshi", "Bowser", "Donkey Kong Jr.", "Toad"]
+
+results.remove("Donkey Kong Jr.")
+results.insert(0, "Donkey Kong Jr.")
+results.reverse()
+
+print(results)
+
+
+
+#results = ["Mario", "Luigi"]
+
+#results.append("Princess")
+#results.append("Yoshi")
+
+#results.append(["Bowser", "Donkey Kong Jr."])
+#results.remove(["Bowser", "Donkey Kong Jr."])
+#results.extend(["Bowser", "Donkey Kong Jr."])
+
+#print(results)
