0cec03e974c03b14b4f8a6c27eb7a2ca6a658e91
Mariia Strykalo <miam.versa@gmail.com>
2025-08-24 19:07:45 +0300

    Create while_outputs_list.py

---
+++ loops_study/while_base/list_base/while_outputs_list.py
@@ -0,0 +1,6 @@
+princesses = ["Snow White", "Cinderella", "Aurora", "Ariel", "Belle", "Jasmine", "Pocahontas", "Mulan", "Tiana", "Rapunzel", "Merida", "Moana", "Raya"]
+index = 0
+
+while index < len(princesses):
+    print(princesses[index])
+    index = index + 1
