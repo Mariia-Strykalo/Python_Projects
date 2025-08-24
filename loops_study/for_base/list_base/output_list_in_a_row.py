1e9d8b12e25a80da216bdab56b0a795ea30528d9
Mariia Strykalo <miam.versa@gmail.com>
2025-08-24 18:58:25 +0300

    Create output_list_in_a_row.py

---
+++ loops_study/for_base/list_base/output_list_in_a_row.py
@@ -0,0 +1,3 @@
+princesses = ["Snow White", "Cinderella", "Aurora", "Ariel", "Belle", "Jasmine", "Pocahontas", "Mulan", "Tiana", "Rapunzel", "Merida", "Moana", "Raya"]
+
+print(", ".join(princesses))
