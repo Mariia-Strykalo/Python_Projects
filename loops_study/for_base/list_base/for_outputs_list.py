a0018c15c0da6d211dbfab6c6012416af55e2130
Mariia Strykalo <miam.versa@gmail.com>
2025-08-24 18:55:45 +0300

    Create for_outputs_list.py

---
+++ loops_study/for_base/list_base/for_outputs_list.py
@@ -0,0 +1,4 @@
+princesses = ["Snow White", "Cinderella", "Aurora", "Ariel", "Belle", "Jasmine", "Pocahontas", "Mulan", "Tiana", "Rapunzel", "Merida", "Moana", "Raya"]
+
+for i in princesses:
+    print(i)
