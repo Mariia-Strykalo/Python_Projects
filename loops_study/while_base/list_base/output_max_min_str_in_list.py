306903d2dee9105e49e7e7ab6fb4035a59eb8720
Mariia Strykalo <miam.versa@gmail.com>
2025-08-25 22:07:35 +0300

    Create output_max_min_str_in_list.py

---
+++ loops_study/while_base/list_base/output_max_min_str_in_list.py
@@ -0,0 +1,42 @@
+#option 1
+princesses = ["Snow White", "Cinderella", "Aurora", "Ariel", "Belle", "Jasmine", "Pocahontas", "Mulan", "Tiana", "Rapunzel", "Merida", "Moana", "Raya", "Chic"]
+index = 0
+min_length = float('inf')
+shortest_word = None
+
+
+while len(princesses) > index:
+        any_string = princesses[index]
+        if min_length > len(any_string):
+            min_length = len(any_string)
+            shortest_word = any_string
+        index = index + 1
+
+
+index = 0
+shortests_word = []
+while index < len(princesses):
+    any_word = princesses[index]
+    if len(any_word) == len(shortest_word):
+        shortests_word.append(any_word)
+    index = index + 1
+
+
+print(", ".join(shortests_word))
+
+
+
+#option 2
+princesses = ["Snow White", "Cinderella", "Aurora", "Ariel", "Belle", "Jasmine", "Pocahontas", "Mulan", "Tiana", "Rapunzel", "Merida", "Moana", "Raya", "Chic"]
+index = 0
+
+min_length = len(min(princesses, key=len))
+
+shortests_word = []
+while index < len(princesses):
+    any_string = princesses[index]
+    if len(any_string) == min_length:
+        shortests_word.append(any_string)
+    index = index + 1
+
+print(", ".join(shortests_word))
