7cb3e882e1e537a03a228beaba058829e8c06f36
Mariia Strykalo <miam.versa@gmail.com>
2025-08-30 22:56:51 +0300

    Create dict_outputs_keys_and_values.py

---
+++ loops_study/dict_base/dict_outputs_keys_and_values.py
@@ -0,0 +1,12 @@
+#outputs keys
+princesses = {"Snow White": "Black", "Cinderella": "Blonde", "Aurora": "Blonde", "Ariel": "Red", "Belle": "Brown", "Jasmine": "Black", "Pocahontas": "Black", "Mulan": "Black", "Tiana": "Black", "Rapunzel": "Blonde", "Merida": "Red", "Moana": "Brown", "Raya": "Black"}
+
+for princess in princesses:
+    print(princess)
+
+
+#outputs values
+princesses = {"Snow White": "Black", "Cinderella": "Blonde", "Aurora": "Blonde", "Ariel": "Red", "Belle": "Brown", "Jasmine": "Black", "Pocahontas": "Black", "Mulan": "Black", "Tiana": "Black", "Rapunzel": "Blonde", "Merida": "Red", "Moana": "Brown", "Raya": "Black"}
+
+for princess in princesses:
+    print(princesses[princess])
